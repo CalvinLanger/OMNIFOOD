a52d770d4709a99f486501aba38c7bde1183d9ec
Calvin Langer <calvin.langerr@gmail.com>
2022-03-05 20:45:49 +0100

    Add smooth scrollbar

---
+++ app.js
@@ -0,0 +1,48 @@
+// CURRENT YEAR
+yearEl = document.querySelector(".current-year");
+currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+// MOBILE NAVIGATION
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// SMOOTH SCROLLING ANIMATION
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // SCROLL BACK TO TOP
+    if (href === "#")
+      widnow.scrollTop({
+        top: 0,
+        behavior: "smooth",
+      });
+  });
+});
+
+// FIXING FLEXBOX GAP MISSING IN SOME SAFARI VERSION
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
